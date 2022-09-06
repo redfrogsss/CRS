c99dd9ae31c2f94e9ec6781e229fd6a23aef97db
Jacky Fan <mrf1013@outlook.com>
2022-09-06 12:06:16 +0800

    feat: implement login page layout

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,23 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginPage() {
+
+    const navigate = useNavigate(); 
+
+    const onSubmitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        // TODO: implement login with database
+        navigate("/home");
+    }
 
     const onClickHandler = (item = "") => {
         if(item === "") {
             return;
         }
-        if(item === "Login"){
-            // window.location = "";
+        if(item === "signin") {
+            navigate("/signin");
         }
     }
 
@@ -21,7 +33,7 @@ export default function LoginPage() {
                 </div>
             </div>
             <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 justify-center justify-self-center">
-                <form className="space-y-6" action="#">
+                <form className="space-y-6" onSubmit={onSubmitHandler}>
                     <h5 className="text-xl font-medium text-gray-900 dark:text-white">
                         Sign in
                     </h5>
@@ -64,15 +76,14 @@ export default function LoginPage() {
                         <svg className="mr-2 -ml-1 w-6 h-6" fill="none" role="img" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path></svg>
                         Sign In
                     </button>
-                    <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                        Not registered?{" "}
-                        <a
-                            href="#"
-                            className="text-blue-700 hover:underline dark:text-blue-500"
-                        >
-                            Create account
-                        </a>
-                    </div>
+                    <button
+                        type="button"
+                        onClick={()=>{onClickHandler("signin")}}
+                        className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 mr-2 mb-2 inline-flex justify-center justify-self-center"
+                    >
+                        <svg className="mr-2 -ml-1 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                        Register
+                    </button>
                 </form>
             </div>
         </div>
