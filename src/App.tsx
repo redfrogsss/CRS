6a589c15c9e6092d12b10638def84820d0382020
Jacky Fan <mrf1013@outlook.com>
2022-09-06 13:29:50 +0800

    style: navbar of the left panel

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route index element={<Navigate to="/login" replace />} />
                 <Route path="login" element={<LoginPage />} />
                 <Route path="register" element={<RegisterPage />} />
+                <Route path="home" element={<HomePage />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
---
+++ src/components/navbar/NewConversationButton.tsx
@@ -0,0 +1,31 @@
+export default function NewConversationButton() {
+    const onClickHandler = () => {
+        // do something
+    };
+
+    return (
+        <button
+            type="button"
+            onClick={() => {
+                onClickHandler();
+            }}
+            className="text-black hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 text-center inline-flex items-center mr-2 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 my-2"
+        >
+            <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                ></path>
+            </svg>
+            <span className="sr-only">New Conversation</span>
+        </button>
+    );
+}
