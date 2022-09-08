c01b565cc5fe6e23f90203ad7d294076b3d40b2b
Jacky Fan <mrf1013@outlook.com>
2022-09-08 21:21:48 +0800

    fix: button text and icon

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -68,33 +68,38 @@ export default function HomePage() {
                 {/* chatbox part in right panel */}
                 <div className="h-[8%]">
                     <div className="h-full w-full p-4">
-                        <input
-                            type="text"
-                            id="base-input"
-                            placeholder="say something..."
-                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[calc(100%-114px-4px-8px)] px-2.5 py-[9px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 inline-block align-top"
-                        />
-                        <button
-                            type="submit"
-                            className="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 ml-2 inline-flex justify-center justify-self-center w-114px"
+                        <form
+                            onSubmit={() => {
+                                return false;
+                            }}
                         >
-                            <svg
-                                className="mr-2 -ml-1 w-6 h-6"
-                                fill="none"
-                                role="img"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
+                            <input
+                                type="text"
+                                id="base-input"
+                                placeholder="say something..."
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[calc(100%-114px-4px-8px)] px-2.5 py-[9px] dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 inline-block align-top"
+                            />
+                            <button
+                                type="submit"
+                                className="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 ml-2 inline-flex justify-center justify-self-center w-114px"
                             >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                                ></path>
-                            </svg>
-                            Sign In
-                        </button>
+                                <svg
+                                    className="mr-2 w-6 h-6"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                                    ></path>
+                                </svg>
+                                Send
+                            </button>
+                        </form>
                     </div>
                 </div>
                 {/* end of chatbox part in right panel */}
