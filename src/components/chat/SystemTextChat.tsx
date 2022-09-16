94c6f50472b3ae400bc7b256d1289b9dcab09e72
Jacky Fan <mrf1013@outlook.com>
2022-09-16 12:32:25 +0800

    style: remove onhover style

--- src/components/chat/SystemTextChat.tsx
+++ src/components/chat/SystemTextChat.tsx
@@ -1,7 +1,7 @@
 export default function SystemTextChat () {
     return (
         <div className="flex justify-start px-8 py-2">
-            <div className="block px-4 pt-4 py-2 max-w-md rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 bg-blue-300">
+            <div className="block px-4 pt-4 py-2 max-w-md rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 bg-blue-300">
                 <p className="font-normal text-gray-700 dark:text-gray-400 break-words">
                     Here are the biggest enterprise technology
                     acquisitions of 2021 so far, in reverse
