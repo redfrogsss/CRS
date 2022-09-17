5e4d772e992b51d559e80455ecfc3c6df363b737
Jacky Fan <mrf1013@outlook.com>
2022-09-17 14:23:39 +0800

    feat: add sample code for form submit

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
-import os
-from flask import Flask, jsonify
+import os   # for os.getenv()
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-
+# Start of Sample Code
 @app.route("/")
 def hello():
     return "Hello, World!"
@@ -16,6 +16,13 @@ def chat_get(name: str):
 @app.route("/chat/<string:name>", methods=["POST"])
 def chat_post(name: str):
     return jsonify({"data": "Hello World", "method": "POST", "name": name})
+
+
+@app.route("/chat/submit", methods=["POST"])
+def chat_form_submit():
+    data = request.args.get("data")
+    return jsonify({"data": data, "method": "POST"})
+# End of Sample Code
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
