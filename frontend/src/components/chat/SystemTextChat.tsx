f82ea911a6f39ec7a663a5b445f5a6aa0fbb7bd7
Jacky FAN <mrf1013@outlook.com>
2022-10-15 11:49:52 +0800

    fix: new line in system message

--- frontend/src/components/chat/SystemTextChat.tsx
+++ frontend/src/components/chat/SystemTextChat.tsx
@@ -7,7 +7,7 @@ export default function SystemTextChat({ content, timestamp }: SystemTextChatInt
     return (
         <div className="flex justify-start px-8 py-2">
             <div className="block px-4 pt-4 py-2 max-w-md rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 bg-blue-300">
-                <p className="font-normal text-gray-700 dark:text-gray-400 break-words">
+                <p className="font-normal text-gray-700 dark:text-gray-400 break-words whitespace-pre-line">
                     {content}
                 </p>
                 <p className="text-sm text-gray-400 text-right">{timestamp}</p>
