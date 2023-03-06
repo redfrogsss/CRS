867e059324726c44f240047e1189d76763928271
Jacky FAN <mrf1013@outlook.com>
2023-03-06 21:42:25 +0800

    UI: center login page and reg page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -20,9 +20,9 @@ export default function LoginPage({
     let [searchParams, setSearchParams] = useSearchParams();
     const [regSuccessMessage, setRegSuccessMessage] = useState("");
 
-    useEffect(()=> {
+    useEffect(() => {
         // show register success
-        console.log("regSuccess = " , searchParams.get("regSuccess"))
+        console.log("regSuccess = ", searchParams.get("regSuccess"))
 
         let regSuccess = searchParams.get("regSuccess");
 
@@ -89,93 +89,95 @@ export default function LoginPage({
     };
 
     useEffect(() => {
-        if(currentUserID !== undefined) {
+        if (currentUserID !== undefined) {
             navigate("/home");
         }
     }, [])
 
     return (
-        <div className="bg-slate-300 w-[100%] h-[100vh] grid grid-cols-2 gap-4 content-center place-content-around">
-            <div className="flex h-full w-[80%] justify-center justify-self-center text-center align-middle">
-                <WebTitle />
-            </div>
-            <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-lg sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 justify-center justify-self-center">
-                <form className="space-y-6" onSubmit={onSubmitHandler}>
-                    <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-                        Sign in
-                    </h5>
-                    <EmailField
-                        onChangeHandler={onEmailChangeHandler}
-                        value={email}
-                    />
-                    <PasswordField
-                        onChangeHandler={onPasswordChangeHandler}
-                        value={password}
-                    />
-                    <button
-                        type="submit"
-                        className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 mr-2 mb-2 inline-flex justify-center justify-self-center"
-                    >
-                        <svg
-                            className="mr-2 -ml-1 w-6 h-6"
-                            fill="none"
-                            role="img"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                            ></path>
-                        </svg>
-                        Sign In
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => {
-                            onClickHandler("signin");
-                        }}
-                        className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 mr-2 mb-2 inline-flex justify-center justify-self-center"
-                    >
-                        <svg
-                            className="mr-2 -ml-1 w-6 h-6"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                            ></path>
-                        </svg>
-                        Register
-                    </button>
-                </form>
-                <div className="mt-2">
-                    {errorMessage.length === 0 ? (
-                        <></>
-                    ) : (
-                        <WarningToast
-                            content={errorMessage}
-                            closeHandler={() => {
-                                setErrorMessage("");
-                            }}
+        <div className="bg-slate-300 w-full h-[100vh]">
+            <div className="w-[80%] h-full grid grid-cols-5 gap-4 content-center place-content-around flex m-auto">
+                <div className="flex h-full w-full text-center col-span-3">
+                    <WebTitle />
+                </div>
+                <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-lg sm:p-6 md:p-8 justify-self-center col-span-2">
+                    <form className="space-y-6" onSubmit={onSubmitHandler}>
+                        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+                            Sign in
+                        </h5>
+                        <EmailField
+                            onChangeHandler={onEmailChangeHandler}
+                            value={email}
                         />
-                    )}
-                    {regSuccessMessage.length === 0 ? (<></>) : (
-                        <RegisterSuccessToast 
-                            content={regSuccessMessage}
-                            closeHandler={()=>{
-                                setRegSuccessMessage("");
-                            }}
+                        <PasswordField
+                            onChangeHandler={onPasswordChangeHandler}
+                            value={password}
                         />
-                    )}
+                        <button
+                            type="submit"
+                            className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 mr-2 mb-2 inline-flex justify-center justify-self-center"
+                        >
+                            <svg
+                                className="mr-2 -ml-1 w-6 h-6"
+                                fill="none"
+                                role="img"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                                ></path>
+                            </svg>
+                            Sign In
+                        </button>
+                        <button
+                            type="button"
+                            onClick={() => {
+                                onClickHandler("signin");
+                            }}
+                            className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 mr-2 mb-2 inline-flex justify-center justify-self-center"
+                        >
+                            <svg
+                                className="mr-2 -ml-1 w-6 h-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                                ></path>
+                            </svg>
+                            Register
+                        </button>
+                    </form>
+                    <div className="mt-2">
+                        {errorMessage.length === 0 ? (
+                            <></>
+                        ) : (
+                            <WarningToast
+                                content={errorMessage}
+                                closeHandler={() => {
+                                    setErrorMessage("");
+                                }}
+                            />
+                        )}
+                        {regSuccessMessage.length === 0 ? (<></>) : (
+                            <RegisterSuccessToast
+                                content={regSuccessMessage}
+                                closeHandler={() => {
+                                    setRegSuccessMessage("");
+                                }}
+                            />
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -65,65 +65,67 @@ export default function RegisterPage({
     };
 
     return (
-        <div className="bg-slate-300 w-[100%] h-[100vh] grid grid-cols-2 gap-4 content-center place-content-around">
-            <div className="flex h-full w-[80%] justify-center justify-self-center text-center align-middle">
-                <WebTitle />
-            </div>
-            <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-lg sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 justify-center justify-self-center">
-                <form
-                    className="space-y-6"
-                    onSubmit={(e) => {
-                        onSubmitHandler(e);
-                    }}
-                >
-                    <h5 className="text-xl font-medium text-gray-900 dark:text-white">
-                        Register
-                    </h5>
-                    <EmailField
-                        onChangeHandler={onEmailChangeHandler}
-                        value={email}
-                    />
-                    <UsernameField
-                        onChangeHandler={onUsernameChangeHandler}
-                        value={username}
-                    />
-                    <PasswordField
-                        onChangeHandler={onPasswordChangeHandler}
-                        value={password}
-                    />
-                    <button
-                        type="submit"
-                        className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 mr-2 mb-2 inline-flex justify-center justify-self-center"
+        <div className="bg-slate-300 w-[100%] h-[100vh]">
+            <div className="w-[80%] h-full grid grid-cols-5 gap-4 content-center place-content-around flex m-auto">
+                <div className="flex h-full w-full text-center col-span-3">
+                    <WebTitle />
+                </div>
+                <div className="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-lg sm:p-6 md:p-8 justify-self-center col-span-2">
+                    <form
+                        className="space-y-6"
+                        onSubmit={(e) => {
+                            onSubmitHandler(e);
+                        }}
                     >
-                        <svg
-                            className="mr-2 -ml-1 w-6 h-6"
-                            fill="none"
-                            role="img"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
-                            ></path>
-                        </svg>
-                        Register
-                    </button>
-                </form>
-                <div className="mt-2">
-                    {errorMessage.length === 0 ? (
-                        <></>
-                    ) : (
-                        <WarningToast
-                            content={errorMessage}
-                            closeHandler={() => {
-                                setErrorMessage("");
-                            }}
+                        <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+                            Register
+                        </h5>
+                        <EmailField
+                            onChangeHandler={onEmailChangeHandler}
+                            value={email}
                         />
-                    )}
+                        <UsernameField
+                            onChangeHandler={onUsernameChangeHandler}
+                            value={username}
+                        />
+                        <PasswordField
+                            onChangeHandler={onPasswordChangeHandler}
+                            value={password}
+                        />
+                        <button
+                            type="submit"
+                            className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 mr-2 mb-2 inline-flex justify-center justify-self-center"
+                        >
+                            <svg
+                                className="mr-2 -ml-1 w-6 h-6"
+                                fill="none"
+                                role="img"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                                ></path>
+                            </svg>
+                            Register
+                        </button>
+                    </form>
+                    <div className="mt-2">
+                        {errorMessage.length === 0 ? (
+                            <></>
+                        ) : (
+                            <WarningToast
+                                content={errorMessage}
+                                closeHandler={() => {
+                                    setErrorMessage("");
+                                }}
+                            />
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
