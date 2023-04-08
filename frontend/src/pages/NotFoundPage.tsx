f37929a62f33bd3c46f14a3ccc3eeab506949eb6
Jacky FAN <mrf1013@outlook.com>
2023-04-08 19:43:50 +0800

    fix: update UI for not found page
    - center the message
    - add link to back to home page

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,19 +1,22 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function NotFoundPage() {
 
     const location = useLocation();
 
     return (
-        <div className="bg-slate-300 w-[100%] h-[100vh] content-center place-content-around justify-center justify-self-center">
+        <div className="bg-slate-300 w-[100%] h-[100vh] flex content-center place-content-around justify-center justify-self-center">
             <div className="p-4 w-full m-auto max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 justify-center justify-self-center">
                 <h5 className="text-xl font-medium text-gray-900 dark:text-white">
                     Page Not Found.
                 </h5>
                 <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                    Are you sure you are in the correct page?
+                    <p className="my-2">
+                        Are you sure you are in the correct page? <br />
+                        <Link to="/" className="text-sky-600 underline">Back to Home Page</Link>
+                    </p>
                     <div className="text-xs">
-                        (pathname: {location.pathname})
+                        (Current Pathname: {location.pathname})
                     </div>
                 </div>
             </div>
